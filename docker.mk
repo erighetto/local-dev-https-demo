34ef4e9d1ec13092357530fbd39fcb95e8b91341
Emanuel Righetto <posta@emanuelrighetto.it>
2020-02-03 07:43:30 +0100

    reformat code

---
+++ docker.mk
@@ -0,0 +1,53 @@
+## up	:	Start up containers.
+.PHONY: up
+up:
+	@echo "Starting up containers for $(PROJECT_NAME)..."
+	docker-compose pull
+	docker-compose up -d --remove-orphans
+
+## down	:	Stop containers.
+.PHONY: down
+down: stop
+
+## start	:	Start containers without updating.
+.PHONY: start
+start:
+	@echo "Starting containers for $(PROJECT_NAME) from where you left off..."
+	@docker-compose start
+
+## stop	:	Stop containers.
+.PHONY: stop
+stop:
+	@echo "Stopping containers for $(PROJECT_NAME)..."
+	@docker-compose stop
+
+## prune	:	Remove containers and their volumes.
+##		You can optionally pass an argument with the service name to prune single container
+##		prune db	: Prune `db` container and remove its volumes.
+##		prune db app	: Prune `db` and `app` containers and remove their volumes.
+.PHONY: prune
+prune:
+	@echo "Removing containers for $(PROJECT_NAME)..."
+	@docker-compose down -v $(filter-out $@,$(MAKECMDGOALS))
+
+## ps	:	List running containers.
+.PHONY: ps
+ps:
+	@docker ps --filter name='$(PROJECT_NAME)*'
+
+## shell	:	Access `app` container via shell.
+.PHONY: shell
+shell:
+	docker exec -ti -e COLUMNS=$(shell tput cols) -e LINES=$(shell tput lines) $(shell docker ps --filter name='$(PROJECT_NAME)_app' --format "{{ .ID }}") sh
+
+## logs	:	View containers logs.
+##		You can optinally pass an argument with the service name to limit logs
+##		logs app	: View `app` container logs.
+##		logs db app	: View `db` and `app` containers logs.
+.PHONY: logs
+logs:
+	@docker-compose logs -f $(filter-out $@,$(MAKECMDGOALS))
+
+# https://stackoverflow.com/a/6273809/1826109
+%:
+	@:
